dc9f9fe3a99bf968c8419980310d238a88aeaac6
jan.cibulka <jan.cibulka@hotmail.cz>
2021-03-09 11:51:15 +0100

    fix instructor page

--- src/pages/instructor.tsx
+++ src/pages/instructor.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-
+import aneta from "../images/aneta.jpg"
 export class Instructor extends React.Component {
   render(): JSX.Element {
     return (
 
       <div className="textBox">
-        <img src={""} style={{ float: "left", marginRight: 20 }} alt="Aneta Mašátová" />
+        <img src={aneta} style={{ float: "left", marginRight: 20, width: 300 }} alt="Aneta Mašátová" />
         <p><b>Aneta Mašátová</b></p>
         <p>Tanci se věnuji již od 3 let. Byla jsem součástí plzeňských závodních týmů. Za tu dobu jsem měla možnost vyzkoušet si spoustu tanečních stylů,
         jako například disco dance, latina, moderna, contenporary, street dance a hip hop. V posledních letech se věnuji převážně tanečnímu stylu
