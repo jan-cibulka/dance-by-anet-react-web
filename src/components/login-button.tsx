89276f5adad1d57e540528add3b780cfc63b0210
jan.cibulka <jan.cibulka@hotmail.cz>
2021-03-15 15:17:47 +0100

    cursor

--- src/components/login-button.tsx
+++ src/components/login-button.tsx
@@ -6,12 +6,13 @@ import { useAuth0 } from "@auth0/auth0-react";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
-    <button
+    <a
       className="nav-link"
+      style={{cursor: "pointer"}}
       onClick={() => loginWithRedirect()}
     >
       Přihlášení
-    </button>
+    </a>
   );
 };
 
