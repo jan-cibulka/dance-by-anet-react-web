c0f21a0b69e494a129f5ad02ef459be67ec102aa
jan.cibulka <jan.cibulka@hotmail.cz>
2021-04-16 09:34:40 +0200

    whitelist

--- src/util/userWhitelist.ts
+++ src/util/userWhitelist.ts
@@ -2,4 +2,4 @@ export function isUserAdmin(userEmail: string):boolean{
     return whiteList.some(x => x==userEmail);
 }
 
-const whiteList= ["wastless1597@gmail.com", "asdqweasd@tadfgfad.com"]
+const whiteList= ["wastless1597@gmail.com", "anet.Cibulková@gmail.com"]
