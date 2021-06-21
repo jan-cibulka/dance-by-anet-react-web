44ddbda5eae19475444e1227e71b94d3e8d0fa6c
jan.cibulka <jan.cibulka@hotmail.cz>
2021-06-21 14:09:20 +0200

    no message

--- src/util/userWhitelist.ts
+++ src/util/userWhitelist.ts
@@ -1,5 +1,7 @@
-export function isUserAdmin(userEmail: string):boolean{
-    return whiteList.some(x => x==userEmail);
+import { User } from "@auth0/auth0-react/dist/auth-state";
+
+export function isUserAdmin(user: User| undefined):boolean{
+    return whiteList.some(x => x==user.email);
 }
 
 const whiteList= ["wastless1597@gmail.com", "anet.Cibulková@gmail.com"]
