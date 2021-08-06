b79d0fea2fd3ffaaab479a6d312956d20fc8ed89
jan.cibulka <jan.cibulka@hotmail.cz>
2021-08-06 11:34:46 +0200

    change to functional components

--- src/pages/account.tsx
+++ src/pages/account.tsx
@@ -1,30 +1,25 @@
 import React from "react"
-import { withAuth0 } from "@auth0/auth0-react"
+import { useAuth0 } from "@auth0/auth0-react";
 import { Redirect } from "react-router-dom";
 
 
 
-export class Account extends React.Component<{ auth0: any }> {
-
-
-  
-
-
-  render(): JSX.Element {
-    if (this.props.auth0.isAuthenticated) {
-      return (
-        <div className="textBox">
-          Účet : {this.props.auth0.user.nickname ? <>{this.props.auth0.user.nickname}</> : <>{this.props.auth0.user.name}</>}
-          <br />
-          Email : {this.props.auth0.user.email}
-          <br />
-        
-        </div>
-      )
-    }
-    return <Redirect to="/" />;
+const Account = () => {
+  const { user, isAuthenticated } = useAuth0();
+  if (isAuthenticated) {
+    return (
+      <div className="textBox">
+        Účet : {user.nickname ? <>{user.nickname}</> : <>{user.name}</>}
+        <br />
+        Email : {user.email}
+        <br />
+      </div>
+    )
   }
+
+  return <Redirect to="/" />;
+
 }
-export default withAuth0(Account)
+export default Account
 
 
--- src/pages/instructor.tsx
+++ src/pages/instructor.tsx
@@ -1,24 +1,19 @@
-import { withAuth0 } from "@auth0/auth0-react";
 import React from "react"
 import aneta from "../images/aneta.jpg"
 
-export class Instructor extends React.Component<{auth0: any}> {
-  render(): JSX.Element {
-    console.log(this.props.auth0);
-    return (
-
-      <div className="textBox">
-        <img src={aneta} style={{ float: "left", marginRight: 20, width: 300 }} alt="Aneta Mašátová" />
-        <p><b>Aneta Mašátová</b></p>
-        <p>Tanci se věnuji již od 3 let. Byla jsem součástí plzeňských závodních týmů. Za tu dobu jsem měla možnost vyzkoušet si spoustu tanečních stylů,
+const Instructor = () => {
+  return (
+    <div className="textBox">
+      <img src={aneta} style={{ float: "left", marginRight: 20, width: 300 }} alt="Aneta Mašátová" />
+      <p><b>Aneta Mašátová</b></p>
+      <p>Tanci se věnuji již od 3 let. Byla jsem součástí plzeňských závodních týmů. Za tu dobu jsem měla možnost vyzkoušet si spoustu tanečních stylů,
         jako například disco dance, latina, moderna, contenporary, street dance a hip hop. V posledních letech se věnuji převážně tanečnímu stylu
         Dancehall, který vychází z jamajské kultury. Součástí Dancehallu je již známý Twerk, kterému se věnuji cca 5 let, kdy jsem tancovala za
-              plzeňskou vystupující skupinu.</p>
-        <p>Ohledně trenérských služeb nabízím kromě svých lekcí také choreografii maturitních předtančení, různá taneční vystoupení a individuální
-            tréninky a poradenství v oblasti tance a flexibility.</p>
-      </div>
-    )
-  }
+        plzeňskou vystupující skupinu.</p>
+      <p>Ohledně trenérských služeb nabízím kromě svých lekcí také choreografii maturitních předtančení, různá taneční vystoupení a individuální
+        tréninky a poradenství v oblasti tance a flexibility.</p>
+    </div>
+  )
 }
 
-export default withAuth0(Instructor)
+export default Instructor
